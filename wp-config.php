c19e0934f18091606e47d3f556c9a8cde15288a2
Aleksei Odegov <aleksei@weirix.ee>
2017-05-30 18:43:32 +0300

    idcard varification

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'estoniancrafts2');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'a');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
