d294a75ac6d270be87c25f9552fa67a84925d1e7
Erik Kesa <erik@dolmit.com>
2017-08-09 20:11:42 +0300

    logout

--- wp-content/themes/estoniancrafts/header.php
+++ wp-content/themes/estoniancrafts/header.php
@@ -74,7 +74,7 @@
 		$fullName = $user->first_name . ' ' . $user->last_name;
 ?>
  	<a href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>" title="<?php _e('My Account','woothemes'); ?>"><?=$fullName ?></a>
-	<a href="<?php echo wp_logout_url(home_url()); ?>" title="<?php _e('My Account','woothemes'); ?>"><?=$fullName ?></a>
+	<a href="<?php echo wp_logout_url(home_url()); ?>" title="<?php _e('Log out','woothemes'); ?>"><?php _e('Log out','woothemes') ?></a>
 
  <?php } 
  else { ?>
