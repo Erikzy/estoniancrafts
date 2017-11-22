ca8d526269def6cd7ce3a49fcb876ca355ff246e
Erik Kesa <erik@dolmit.com>
2017-11-22 16:44:56 +0200

    lightbox

--- wp-content/themes/basel/js/fixes.js
+++ wp-content/themes/basel/js/fixes.js
@@ -61,6 +61,24 @@ jQuery(document).ready(function(){
 	jQuery('.y-link').click(function(){
 		this.event.preventDefault();
 		alert('cheese');
+		 jQuery.fancybox({
+                        'padding'        : 0,
+                        'autoScale'      : false,
+                        'transitionIn'   : 'none',
+                        'transitionOut'  : 'none',
+                        'title'          : this.title,
+                        'width'          : 680,
+                        'height'         : 495,
+                        'href'           : this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
+                        'type'           : 'swf',
+                        'swf'            : {
+                            'wmode'              : 'transparent',
+                            'allowfullscreen'    : 'true'
+                        }
+                    });
+                return false;
+		
+		
 	});
 
 
