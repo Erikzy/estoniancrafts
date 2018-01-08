766958b090ebdd85b15deef7362eaf00452f4e3e
Erik Kesa <erik@dolmit.com>
2018-01-08 15:10:17 +0200

    seo link fix

--- wp-content/plugins/lb-dokan/lbDokanUser.php
+++ wp-content/plugins/lb-dokan/lbDokanUser.php
@@ -124,9 +124,9 @@ class lbDokanUser {
     	$ext_settings['ec_store_sm_youtube'] = ! empty( $_POST['ec_store_sm_youtube'] ) ? wc_clean( $_POST['ec_store_sm_youtube'] ) : '';
     	$ext_settings['ec_store_sm_instagram'] = ! empty( $_POST['ec_store_sm_instagram'] ) ? wc_clean( $_POST['ec_store_sm_instagram'] ) : '';
     	$ext_settings['ec_store_sm_flickr'] = ! empty( $_POST['ec_store_sm_flickr'] ) ? wc_clean( $_POST['ec_store_sm_flickr'] ) : '';
-		
-		update_user_meta( $store_id, 'user_nicename',  sanitize_title($_POST['dokan_store_name']));
-		
+		$current_user = wp_get_current_user();
+		//update_user_meta( $store_id, 'user_nicename',  sanitize_title($_POST['dokan_store_name']));
+		$user_id = wp_update_user( array( 'ID' => $current_user->ID, 'user_nicename' => sanitize_title($_POST['dokan_store_name']) ) );
 		update_user_meta( $store_id, 'ktt_extended_settings', $ext_settings );
 
 	}
