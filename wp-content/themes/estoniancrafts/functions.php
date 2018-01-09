1160b66196b243efabebcf93c422202bb8b44ab4
Erik Kesa <erik@dolmit.com>
2018-01-09 14:41:56 +0200

    user redirect

--- wp-content/themes/estoniancrafts/functions.php
+++ wp-content/themes/estoniancrafts/functions.php
@@ -52,14 +52,7 @@ function ec_get_sidebar_name()
 
 
  function redirect_to_user_appropriate_home() {
-		$user = wp_get_current_user();
-		var_dump($user->roles);
-		if(in_array('seller', $user->roles)){
-		
-  			return get_site_url(null, 'my-account/dashboard/');
-  		}else{
-  			return get_site_url(null, 'my-account/edit-account/');
-  		}
+		return get_site_url(null, 'user-home-redirect');
 	}
 
 	add_filter('login_redirect','redirect_to_user_appropriate_home');
