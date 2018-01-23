642e84d9a169d74566895f10db14203e7f0a70df
Erik Kesa <erik@dolmit.com>
2018-01-23 03:45:57 +0200

    fix

--- wp-content/themes/estoniancrafts/filters.php
+++ wp-content/themes/estoniancrafts/filters.php
@@ -87,7 +87,7 @@ class EC_Filters
 				throw new Eabi_Woocommerce_Postoffice_Exception("Missing address details!");
 			}
 
-			echo $ddd[0];
+			echo $street->goDoThis();
 			$start = date("Y-m-d",strtotime("tomorrow"))."T12:00:00";
 			$finish = date("Y-m-d",strtotime("tomorrow"))."T15:00:00";
 			$fragile = 0;	
